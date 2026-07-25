8cdb561d20a4ee9d32c4bb16eed7257c87124b6b
suazo <suazo@JoseLopez.lan>
2026-07-24 21:03:55 -0600

    trabajo en clase

--- Hola_Mundo/src/hola_mundo/Hola_Mundo.java
+++ Hola_Mundo/src/hola_mundo/Hola_Mundo.java
@@ -13,8 +13,79 @@ public class Hola_Mundo {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
     
-}
+    // Sirve para comentar una sola linea
+    /* 
+       Pleca Asterisco -> Abrir el bloque de comentario 
+       Asterico Plecas -> Cerrar el bloque de comentario
+       
+       Todo lo de enmedio.... es un comentario
+    
+    */
+    
+public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println("Hola me llamo Jose");
+        System.out.println("Este sera mi 1er");
+        System.out.println("Programa!!!!");
+        System.out.println("");
+        System.out.println("");
+        
+        System.out.print("Vamos camino a ");
+        System.out.print("la clase de ");
+        System.out.print("Progra!!");
+        System.out.println("");
+        System.out.println("");
+        
+        System.out.println("");
+        System.out.printf("Tu apellido es %s ","Lopez");
+        System.out.println("");
+        System.out.printf("%s Tu apellido es","Lopez");    
+        System.out.println("");
+        System.out.printf("Tu apellido %s es","Lopez");   
+        System.out.println("");
+        System.out.println("");
+        
+        
+        System.out.println("");
+        System.out.printf("Tu apellido es %S","Lopez");
+        System.out.println("");
+        System.out.printf("%S Tu apellido es","Lopez");    
+        System.out.println("");
+        System.out.printf("Tu apellido %S es","Lopez");   
+        System.out.println("");
+        System.out.println("");
+        
+        System.out.println("");
+        System.out.printf("Tu apellido es %S\n","Lopez");
+        System.out.printf("%S Tu apellido es\n","Lopez");    
+        System.out.printf("Tu apellido %S es\n","Lopez");   
+        System.out.println("");
+        System.out.println("");
+
+        System.out.println("Hola \n este es \n un nuevo \n amancer");
+        System.out.println("Hola \t este es \t un nuevo \t amancer");
+        System.out.println("");
+        System.out.println("");
+        
+        //Variables
+        int edad;    //Tipo Entero
+        double peso; //Tipo Decimal
+        char inicial; //Tipo Caracter -> letras,digitos,simbolos,simbolos especiales
+        String nombre; //Tipo Cadena
+        boolean estado; //Tipo Estado: true/false
+        
+        edad = 36;
+        peso = 95.25;
+        inicial = 'J';
+        nombre = "Jose";
+        
+        System.out.printf("Tengo %d anios \n",edad);
+        System.out.printf("Mi pedo ideal %.2f \n",peso);
+        System.out.printf("Dime %c por mi inicial\n",inicial);
+        System.out.printf("%s es mi nombre",nombre);
+        
+    }//Fin de Main
+    
+}//Fin de Class
